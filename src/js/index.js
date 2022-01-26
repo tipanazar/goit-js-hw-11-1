2f27cd0042ced58262fca33b3e91d4282f6ab63f
ololosh-tralevich <karpeko76@gmail.com>
2022-01-26 20:52:17 +0200

    gallery is ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 import { axios } from './fetchPhotos';
 import { PixabyAPI } from './fetchPhotos';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import handlebars from '../handlebars/handlebars.hbs';
 
 const userForm = document.querySelector('form#search-form');
-const body = document.querySelector('body');
+const mainSection = document.querySelector('main');
 const textField = userForm.elements.searchQuery;
 
 const pixabyApi = new PixabyAPI();
@@ -11,33 +12,48 @@ const pixabyApi = new PixabyAPI();
 userForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  // body.insertAdjacentHTML("beforeend", handlebars());
-
   const keyword = textField.value;
 
   pixabyApi.keyword = keyword;
   pixabyApi.page = 1;
 
-  // galleryListEl.innerHTML = '';
-
+  
   pixabyApi
-    .fetchPhotos()
-    .then(({ data } = {}) => {
-      if (data.hits.length === 0) {
-        console.log('Не найдено');
-        return;
-      }
-      console.log(data.hits);
-      for (let photo of data.hits) {
-        // console.log(photo);
-        const photoLikes = photo.likes;
-        const photoVievs = photo.vievs;
-        const photoComments = photo.comments;
-        const photoDownloads = photo.downloads;
-        const photoSmall = photo.previevURL;
-        const photoLagre = photo.largeImageURL;
-      }
+  .fetchPhotos()
+  .then(({ data } = {}) => {
+        mainSection.innerHTML = '';
 
+      if (data.hits.length === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        return;
+      } else if (data.hits.length > 0) {
+        console.log(data.hits);
+        for (let photo of data.hits) {
+            console.log(photo)
+          const photoLikes = photo.likes;
+          const photoViews = photo.views;
+          const photoComments = photo.comments;
+          const photoDownloads = photo.downloads;
+          const photoSmall = photo.webformatURL;
+          const photoLarge = photo.largeImageURL;
+          const photoAlt = photo.tags;
+
+          //   console.log(photoLikes);
+
+          mainSection.insertAdjacentHTML(
+            'beforeend',
+            handlebars({
+              photoLikes,
+              photoViews,
+              photoComments,
+              photoDownloads,
+              photoSmall,
+              photoLarge,
+              photoAlt,
+            }),
+          );
+        }
+      }
       // if (data.total_pages === 1) {
       // //   galleryListEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data.results));
       // //   loadMoreBtnEl.classList.add('is-hidden');
